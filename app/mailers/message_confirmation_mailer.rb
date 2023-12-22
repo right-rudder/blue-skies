98d743773d786698eee2e9862b33f7d3269b5e7b
Tim Jedrek <tim@timjedrek.com>
2023-12-22 16:44:33 -0600

    fix mailer

--- app/mailers/message_confirmation_mailer.rb
+++ app/mailers/message_confirmation_mailer.rb
@@ -1,11 +1,15 @@
 class MessageConfirmationMailer < ApplicationMailer
 
-  default from: 'no-reply@pitcairnmailer.com'
+  default from: 'no-reply@flyblueskiesabove.com'
 
   def message_confirmation_email(message)
     @message = message
-
-    mail(to: @message.email, bcc: ["no-reply@pitcairnmailer.com", "flynow@pitcairnflight.com"], reply_to: "flynow@pitcairnflight.com", subject: "✈️ Thank you for contacting Pitcairn Flight Academy LLC")
+    
+    if Rails.env.production?
+      mail(to: @message.email, bcc: ["no-reply@flyblueskiesabove.com", "connect@flytheblueskies.com"], reply_to: "connect@flytheblueskies.com", subject: "✈️ Thank You For Contacting Fly The Blue Skies Professional Flight Instruction")
+    else
+      mail(to: @message.email, bcc: ["no-reply@flyblueskiesabove.com"], reply_to: "connect@flytheblueskies.com", subject: "✈️ Thank You For Contacting Fly The Blue Skies Professional Flight Instruction")
+    end
   end
 
 end
