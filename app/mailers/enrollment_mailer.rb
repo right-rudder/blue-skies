52fbed04a6a05c6669901d84b8a79be1e07d7eab
Tim Jedrek <tim@timjedrek.com>
2023-12-22 19:21:19 -0600

    configure mailer

--- app/mailers/enrollment_mailer.rb
+++ app/mailers/enrollment_mailer.rb
@@ -1,11 +1,15 @@
 class EnrollmentMailer < ApplicationMailer
 
-  default from: 'no-reply@simplifly-co.com'
+  default from: 'no-reply@flyblueskiesabove.com'
 
   def enrollment_email(message)
     @message = message
 
-    mail(to: @message.email, bcc: ["no-reply@simplifly-co.com", "info@simpliflyco.com"], subject: "✈️ Application to Enroll at SimpliFly")
+    if Rails.env.production?
+      mail(to: @message.email, bcc: ["no-reply@idealaviationstlouis.com", "connect@flytheblueskies.com"], reply_to: "connect@flytheblueskies.com", subject: "✈️ Application to Enroll at Blue Skies Above")
+    else
+      mail(to: @message.email, bcc: ["no-reply@idealaviationstlouis.com"], reply_to: "connect@flytheblueskies.com", subject: "✈️ Application to Enroll at Blue Skies Above")
+    end
   end
 
 
