8d048eeebc86484d41507a9ca34451b4c593ad0a
Tim Jedrek <tim@timjedrek.com>
2024-01-17 16:56:25 -0600

    change email.. WHOOPS

--- app/mailers/enrollment_mailer.rb
+++ app/mailers/enrollment_mailer.rb
@@ -6,9 +6,9 @@ class EnrollmentMailer < ApplicationMailer
     @message = message
 
     if Rails.env.production?
-      mail(to: @message.email, bcc: ["no-reply@idealaviationstlouis.com", "connect@flytheblueskies.com"], reply_to: "connect@flytheblueskies.com", subject: "✈️ Application to Enroll at Blue Skies Above")
+      mail(to: @message.email, bcc: ["no-reply@flyblueskiesabove.com", "connect@flytheblueskies.com"], reply_to: "connect@flytheblueskies.com", subject: "✈️ Application to Enroll at Blue Skies Above")
     else
-      mail(to: @message.email, bcc: ["no-reply@idealaviationstlouis.com"], reply_to: "connect@flytheblueskies.com", subject: "✈️ Application to Enroll at Blue Skies Above")
+      mail(to: @message.email, bcc: ["no-reply@flyblueskiesabove.com"], reply_to: "connect@flytheblueskies.com", subject: "✈️ Application to Enroll at Blue Skies Above")
     end
   end
 
