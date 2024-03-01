ba3950d2f7a615bf18bcb00ec5d8217d6c962260
Tim Jedrek <tim@timjedrek.com>
2024-03-01 04:17:44 -0500

    enroll form to crm

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,6 +1,6 @@
 class Enrollment < ApplicationRecord
   before_validation :strip_phone_number
-  #after_save :to_lacrm
+  after_save :to_ghl
 
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -11,65 +11,26 @@ class Enrollment < ApplicationRecord
     self.phone = phone.to_s.gsub(/[-() ]/, "")
   end
 
-  def to_lacrm
-    api_key = ENV['LACRM_API']
-    # Set the endpoint and headers
-    endpoint = "https://api.lessannoyingcrm.com/v2/"
-    headers = {
-      "Authorization" => api_key,
-      "Content-Type" => "application/json"
-    }
-    # Get the user ID
-    user_payload = {
-      "Function" => "GetUser",
-      "Parameters" => {}
-    }
-    user_response = HTTParty.get(endpoint, headers: headers, body: user_payload.to_json)
-    if user_response.code == 200
-      user_id = JSON.parse(user_response.body)['UserId']
-    else
-      # Handle the error
-      user_id = nil
-    end
+  def to_ghl
+    ghl_url = ENV['ghl_enroll']
+    ghl_payload = {
+      "first name" => "#{self.first_name}",
+      "last name" => "#{self.last_name}",
+      "email" => "#{self.email}",
+      "phone" => "#{self.phone}",
+      "notes" => "
+      
+      Interested program: #{self.location}
+      Goals: #{self.goals}
+      Previous training: #{self.previous_training}
+      Comments: #{self.comments}
+      Preferred date: #{self.preferred_date} #{self.preferred_availability}
+      Alternate date: #{self.alternate_date} #{self.alternate_availability}
+      ",
+    }     
+    HTTParty.post(ghl_url, body: ghl_payload.to_json, headers: { "Content-Type" => "application/json" })
+  end
 
-    # Create the contact
-    contact_payload = {
-      "Function" => "CreateContact",
-      "Parameters" => {
-        "IsCompany" => false,
-        "AssignedTo" => user_id,  #need to just get the USER ID so I can avoid doin the get call each time..
-        "Name" => "#{self.first_name} #{self.last_name}",
-        "Email" => "#{self.email}",
-        "Phone" => "#{self.phone}",
-        "Background Info" => "Originated from enrollment form on website",
-      }
-    }
-
-
-    
-
-    response = HTTParty.post(endpoint, headers: headers, body: contact_payload.to_json)
-    contact_id = JSON.parse(response.body)['ContactId']
-    
-
-    # Add a note
-    note_payload = {
-      "Function" => "CreateNote",
-      "Parameters" => {
-        "ContactId" => contact_id,
-        "Note" => "
-          Previous Training: #{self.previous_training}
-          Goals: #{self.goals}
-          Comments: #{self.comments}
-
-          Selected Date: #{self.preferred_date}
-          Selected Time: #{self.preferred_availability}
-          Alternate Date: #{self.alternate_date}
-          Alternate Time: #{self.alternate_availability}
-        ",
-      }
-    }
-    HTTParty.post(endpoint, headers: headers, body: note_payload.to_json)
-
-  end  
+  #:first_name, :last_name, :phone, :email, :goals, :previous_training, :comments, :preferred_date, :preferred_availability, :alternate_date, :alternate_availability, :location
+  
 end
