626383836ffff806f360d7f3465de33e21459fa5
Tim Jedrek <tim@timjedrek.com>
2024-03-01 04:45:55 -0500

    book dl to crm

--- app/mailers/book_downloads_mailer.rb
+++ app/mailers/book_downloads_mailer.rb
@@ -4,8 +4,12 @@ class BookDownloadsMailer < ApplicationMailer
 
   def book_downloads(book_download)
     @book_download = book_download
-
-    mail(to: @book_download.email, bcc: ["no-reply@flyblueskiesabove.com", "connect@flytheblueskies.com"], subject: "✈️ Blue Skies Above FREE Pilot Training Booklet")
+    
+    if Rails.env.production?
+      mail(to: @book_download.email, bcc: ["no-reply@flyblueskiesabove.com", "connect@flytheblueskies.com"], subject: "✈️ Blue Skies Above FREE Pilot Training Booklet")
+    else
+      mail(to: @book_download.email, bcc: ["no-reply@flyblueskiesabove.com"], subject: "✈️ Blue Skies Above FREE Pilot Training Booklet")
+    end
   end
 
 
--- app/models/book_download.rb
+++ app/models/book_download.rb
@@ -1,6 +1,6 @@
 class BookDownload < ApplicationRecord
   before_validation :strip_phone_number
-  after_save :to_lacrm
+  after_save :to_ghl
   
   validates :name, presence: true
   validates :phone, presence: true, format: { with: /\A\d{10}\z/, message: "must be a valid 10-digit phone number" }
@@ -10,43 +10,15 @@ class BookDownload < ApplicationRecord
     self.phone = phone.to_s.gsub(/[-() ]/, "")
   end
 
-  def to_lacrm
-    api_key = ENV['LACRM_API']
-    # Set the endpoint and headers
-    endpoint = "https://api.lessannoyingcrm.com/v2/"
-    headers = {
-      "Authorization" => api_key,
-      "Content-Type" => "application/json"
-    }
-    # Get the user ID
-    user_payload = {
-      "Function" => "GetUser",
-      "Parameters" => {}
-    }
-    user_response = HTTParty.get(endpoint, headers: headers, body: user_payload.to_json)
-    if user_response.code == 200
-      user_id = JSON.parse(user_response.body)['UserId']
-    else
-      # Handle the error
-      user_id = nil
-    end
+  def to_ghl
+    ghl_url = ENV['ghl_book']
+    ghl_payload = {
+      "name" => "#{self.name}",
+      "email" => "#{self.email}",
+      "phone" => "#{self.phone}",
+    }     
+    HTTParty.post(ghl_url, body: ghl_payload.to_json, headers: { "Content-Type" => "application/json" })
+  end
 
-    # Create the contact
-    contact_payload = {
-      "Function" => "CreateContact",
-      "Parameters" => {
-        "IsCompany" => false,
-        "AssignedTo" => user_id,  #need to just get the USER ID so I can avoid doin the get call each time..
-        "Name" => "#{self.name}",
-        "Email" => "#{self.email}",
-        "Phone" => "#{self.phone}",
-        "Background Info" => "Originated from book download form on website",
-      }
-    }
-
-
-    response = HTTParty.post(endpoint, headers: headers, body: contact_payload.to_json)
-
-  end  
-
+  
 end
