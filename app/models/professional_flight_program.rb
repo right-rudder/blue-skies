c47fed4d0f0d711e63cdb5d7b79b95449ae81513
Diego Yon <diegoyon@outlook.com>
2025-02-20 21:54:23 -0600

    add send to portal webhooks

--- app/models/professional_flight_program.rb
+++ app/models/professional_flight_program.rb
@@ -1,6 +1,7 @@
 class ProfessionalFlightProgram < ApplicationRecord
   before_validation :strip_phone_number
   after_save :to_ghl
+  after_save :to_portal
   
   validates :name, presence: true
   validates :phone, presence: true, format: { with: /\A\d{10}\z/, message: "must be a valid 10-digit phone number" }
@@ -24,5 +25,24 @@ class ProfessionalFlightProgram < ApplicationRecord
       "message" => "#{self.message}",
     }     
     HTTParty.post(ghl_url, body: ghl_payload.to_json, headers: { "Content-Type" => "application/json" })
+  end
+
+  def to_portal
+    portal_url = "https://portal.rightruddermarketing.com/api/leads"
+    api_key = ENV['PORTAL_API_KEY']
+    portal_payload = {
+      "first_name": "#{self.name.strip.squeeze(" ").split(" ").first}",
+      "last_name": "#{self.name.strip.squeeze(" ").split(" ").second}",
+      "email": "#{self.email}",
+      "phone": "#{self.phone}",
+      "campaign": "pro_flight",
+      "account_id": 3, #Blue skies account ID
+      "metadata": {
+        "start_date": "#{self.start_date}",
+        "target_date": "#{self.target_date}",
+        "message": "#{self.message}"
+      }
+    }     
+    HTTParty.post(portal_url, body: portal_payload.to_json, headers: { "Content-Type": "application/json", "X-API-Key": api_key })
   end
 end
